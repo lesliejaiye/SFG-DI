2a3d6d7ecd96126131a3088a9069399b0812c1cf
Leslie Jaiyesimi <leslie.jaiyesimi@dell.com>
2023-04-06 10:34:37 -0400

    Dependency Injection, Basic

--- src/main/java/guru/springframework/sfgDI/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgDI/SfgDiApplication.java
@@ -2,12 +2,26 @@ package guru.springframework.sfgDI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.sfgDI.controllers.MyController;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+
+		/*Defines Spring Context */
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		
+		/*Asking Spring to provide an isntance of the Spring Context BEAN */
+		/*Notice 'new' is not here to define object, Spring framewrok assumes 'new' underneath */
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		/*Use object to execute method, set to variable */
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
