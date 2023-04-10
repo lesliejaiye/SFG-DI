053b178c0fab862b4f122f8c6dc2b3011df5bdc9
Leslie Jaiyesimi <leslie.jaiyesimi@dell.com>
2023-04-10 11:36:50 -0400

    DI with SPRING

--- src/main/java/guru/springframework/sfgDI/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgDI/SfgDiApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import guru.springframework.sfgDI.controllers.ConstructorInjectedController;
 import guru.springframework.sfgDI.controllers.MyController;
+import guru.springframework.sfgDI.controllers.PropertyInjectedController;
+import guru.springframework.sfgDI.controllers.SetterInjectedController;
 
 @SpringBootApplication
 public class SfgDiApplication {
@@ -22,6 +25,25 @@ public class SfgDiApplication {
 		String greeting = myController.sayHello();
 
 		System.out.println(greeting);
+
+		System.out.println("-----Property");
+		
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("-----Setter");
+
+		SetterInjectedController setterinjectedcontroller = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+		System.out.println(setterinjectedcontroller.getGreeting());
+
+		System.out.println("-----Constructor");
+
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+		System.out.println(constructorInjectedController.getGreeting());
+
 	}
 
 }
--- src/main/java/guru/springframework/sfgDI/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgDI/controllers/ConstructorInjectedController.java
@@ -1,10 +1,15 @@
 package guru.springframework.sfgDI.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import guru.springframework.sfgDI.services.GreetingService;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
+    @Autowired
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
 
