5c4ea2584d2d2cdd126058cafc0c30fd881d58e5
Leslie Jaiyesimi <leslie.jaiyesimi@dell.com>
2023-04-10 11:36:35 -0400

    DI with SPRING

--- src/main/java/guru/springframework/sfgDI/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/sfgDI/controllers/PropertyInjectedController.java
@@ -1,8 +1,15 @@
 package guru.springframework.sfgDI.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import guru.springframework.sfgDI.services.GreetingService;
 
+@Controller
 public class PropertyInjectedController {
+    
+    /*tells Spring that you want to inject an instance of the greeting service  */
+    @Autowired
     
     public GreetingService greetingService;
 
--- src/main/java/guru/springframework/sfgDI/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/sfgDI/services/GreetingServiceImpl.java
@@ -1,5 +1,10 @@
 package guru.springframework.sfgDI.services;
 
+import org.springframework.stereotype.Service;
+
+/*tells Spring to bring an instance of this into context within the Controller */
+@Service
+
 public class GreetingServiceImpl implements GreetingService{
     @Override
     public String sayGreeting() {
