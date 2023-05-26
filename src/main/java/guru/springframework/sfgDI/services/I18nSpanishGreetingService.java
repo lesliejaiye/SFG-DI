46a6848fc7e9f6cd36456de7999c7eeb5eabb8ff
Leslie Jaiyesimi <leslie.jaiyesimi@dell.com>
2023-05-26 16:38:45 -0400

    commit

--- src/main/java/guru/springframework/sfgDI/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgDI/services/I18nSpanishGreetingService.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgDI.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 //you can input Qualifier in service line
 //Qualifier defined in Service because this script holds no constructor 
 @Service("i18nService")
